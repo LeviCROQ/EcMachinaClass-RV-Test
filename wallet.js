c59fdc0c4e0d31feeccf51a68e96d07f020fa3e9
COrrRoq <catherine.orr@roq.co.uk>
2023-02-09 11:39:39 +0000

    Second push

--- wallet.js
+++ wallet.js
@@ -7,11 +7,14 @@ class Wallet {
 
     rate() {
         //Display the exchange rate of ROVR coin against £ 
+        throw new Error ('Not supported yet');
     }
+
 
     add() {
         //add number to current balance. Prompt the user on how many coins to add.
         //The add must be parsed to a minimum value that they can add to the wallet
+        throw new Error ('Minimum Deposit: ');
     }
 }
 
