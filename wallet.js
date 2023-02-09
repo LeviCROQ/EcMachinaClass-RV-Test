76ff8537c68d6c40978c73f7d7bb039b833ba23a
COrrRoq <catherine.orr@roq.co.uk>
2023-02-09 11:36:40 +0000

    Skeleton node for ROVR wallet

---
+++ wallet.js
@@ -0,0 +1,18 @@
+class Wallet {
+
+    current() {
+        //display current balance of ROVR coin wallet
+        throw new Error('Insufficient funds');
+    }
+
+    rate() {
+        //Display the exchange rate of ROVR coin against £ 
+    }
+
+    add() {
+        //add number to current balance. Prompt the user on how many coins to add.
+        //The add must be parsed to a minimum value that they can add to the wallet
+    }
+}
+
+module.exports = Wallet;
